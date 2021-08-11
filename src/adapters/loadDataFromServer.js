2445691096d06ebb3e1a792e86fda5e1248f15c4
mturich <onlineshop1@outlook.de>
2021-08-11 22:13:02 +0200

    MessageCard css added

--- src/adapters/loadDataFromServer.js
+++ src/adapters/loadDataFromServer.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import './MeaningCards.css';
 
 function GetDataFromServer({ apiURL }) {
   const [meanings, setMeanings] = useState([]);
@@ -30,62 +31,13 @@ function GetDataFromServer({ apiURL }) {
   }, [apiURL]);
 
   const content = meanings.map((meaning, index) => (
-    <div
-      className="Cards"
-      style={{
-        fontFamily: 'Caveat, sans-serif',
-        fontSize: 'clamp(1rem,1.2rem,1.5rem)',
-        display: 'flex',
-        flexDirection: 'column',
-        // width: '40rem',
-        width: '80%',
-        minHeight: '26rem',
-        // minHeight: 'clamp(60%,70%,80%)',
-        maxHeight: '80%',
-        overflowX: 'auto',
-
-        margin: 'auto 6rem',
-
-        backgroundColor: '#FFFF88',
-        border: '1px solid black',
-        scrollSnapAlign: 'center',
-        scrollSnapStop: 'always',
-      }}
-    >
-      <div
-        key={meaning.label}
-        value={meaning.label}
-        className="CardMeaning"
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          width: '100%',
-          height: '15%',
-          margin: 'auto',
-          borderBottom: '1px solid black',
-          fontSize: '1.5rem',
-          textDecoration: 'underline'
-        }}
-      >
-        <span style={{ padding: '1rem',  }}>Meaning {index + 1}:</span>
+    <div className="Cards">
+      <div key={meaning.label} value={meaning.label} className="CardMeaning">
+        <span style={{ padding: '1rem' }}>Meaning {index + 1}:</span>
         <p>{meaning.word}</p>
       </div>
 
-      <div
-        key={meaning.label}
-        value={meaning.label}
-        className="CardOrigin"
-        style={{
-          height: '80%',
-          width: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          overflowX: 'auto',
-          margin: 'auto',
-          padding: ' 0.5em 1em',
-        }}
-      >
+      <div key={meaning.label} value={meaning.label} className="CardOrigin">
         <div style={{ maxHeight: '100%' }}>
           <div style={{ margin: '0' }}>
             {meaning.meanings.map((typ) => (
@@ -105,10 +57,15 @@ function GetDataFromServer({ apiURL }) {
           </p>
           <p style={{ paddingLeft: '1em' }}> [{meaning.phonetic}]</p>
 
-          <p style={{ textDecoration: 'underline', paddingTop: '0.5rem' }} id="origin">
+          <p
+            style={{ textDecoration: 'underline', paddingTop: '0.5rem' }}
+            id="origin"
+          >
             origin:
           </p>
-          <p style={{ paddingLeft: '1em', paddingBottom:'1em' }}>{meaning.origin}</p>
+          <p style={{ paddingLeft: '1em', paddingBottom: '1em' }}>
+            {meaning.origin}
+          </p>
         </div>
       </div>
     </div>
@@ -116,25 +73,7 @@ function GetDataFromServer({ apiURL }) {
 
   return (
     <>
-      {/* <div>
-        <p style={{ padding: '3em 5em 1em', textTransform: 'uppercase' }}>
-          the meaning of the word is
-        </p>
-      </div> */}
-      <div
-        className="wrapper"
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          height: '32rem',
-          width: '90%',
-
-          backgroundColor: '#ebe6e6',
-          overflowX: 'scroll',
-          scrollSnapType: 'x mandatory',
-          margin: '0 auto',
-        }}
-      >
+      <div className="wrapper">
         <div class="dummyDiv" style={style}></div>
         {content}
         <div class="dummyDiv" style={style}></div>
